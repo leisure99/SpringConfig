e471386ba95e28bef4849929c42d3b0261bb5da8
leisure99 <578964701@qq.com>
2018-10-23 17:46:05 +0800

    完善client和server

--- client/src/main/java/com/springclient/client/ClientApplication.java
+++ client/src/main/java/com/springclient/client/ClientApplication.java
@@ -1,12 +1,23 @@
 package com.springclient.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
+    }
+    @Value("${leisure.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
     }
 }
