f4ed1c4698db472a6733641e27acec128c090414
leisure99 <578964701@qq.com>
2018-10-24 15:07:13 +0800

    额外创建一个git仓库来对配置文件进行处理

--- client/src/main/java/com/springclient/client/ClientApplication.java
+++ client/src/main/java/com/springclient/client/ClientApplication.java
@@ -3,21 +3,29 @@ package com.springclient.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
+@RefreshScope
+@EnableDiscoveryClient
 public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
     }
-    @Value("${leisure.hello}")
-    private String hello;
+    @Value("${leisure}")
+    String foo;
 
-    @RequestMapping("/hello")
-    public String from() {
-        return this.hello;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
     }
+
 }
--- server/src/main/java/com/springserver/server/ServerApplication.java
+++ server/src/main/java/com/springserver/server/ServerApplication.java
@@ -2,10 +2,14 @@ package com.springserver.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
+@EnableEurekaClient
 public class ServerApplication {
 
     public static void main(String[] args) {
